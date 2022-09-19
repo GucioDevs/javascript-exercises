2f7914dbda5afa9f90cff72a313d8d88f32f207c
August Dominik Raaschou-Pedersen <guciodevs@gmail.com>
2022-09-19 23:31:01 +0200

    Finish ex 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof(num1) != "number" || typeof(num2) != "number")
+        return "ERROR"
 
+    result = 0;
+    
+    for(let i = num1; num1 < num2 ? i <= num2 : i >= num2 ; num1 < num2 ? i++ : i--){
+        result = result + i;
+    }
+    return result;
 };
 
 // Do not edit below this line
