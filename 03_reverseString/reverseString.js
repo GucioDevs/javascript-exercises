48ee99ea4f2cf42a3bfd5103e98fa56e3550ed09
August Dominik Raaschou-Pedersen <guciodevs@gmail.com>
2022-09-19 19:55:04 +0200

    Finish ex 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    arr = input.split("");
+    result = "";
+    for(let i = 0; i < arr.length;i++){
+        result = result + arr[arr.length - 1 - i];
+    }
+    return result;
 };
 
 // Do not edit below this line
