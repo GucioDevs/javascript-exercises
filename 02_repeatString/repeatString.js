cbf938a8957f435e157e191c24e3f0b763b30d3a
August Dominik Raaschou-Pedersen <guciodevs@gmail.com>
2022-09-19 19:28:24 +0200

    Finish ex 01 and ex 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if(num < 0)
+        return "ERROR"
+        
+    result = "";
+    for(let i = 0; i < num; i++){
+        result = result + str;
+    }
+    return result;
 };
 
 // Do not edit below this line
