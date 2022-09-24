e8e48b3dfeeecaa8fdb80f1bfe07ffad454daaf8
August Dominik Raaschou-Pedersen <guciodevs@gmail.com>
2022-09-24 09:43:38 +0200

    Solved 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const strLetters = alphaOnly(str).toLowerCase();
+    const reversed = Array.from(strLetters).reverse().join('');
+    return reversed === strLetters;    
 };
 
+function alphaOnly(str){
+    return str.replace(/[^A-Za-z]/g, '');
+}
 // Do not edit below this line
 module.exports = palindromes;
