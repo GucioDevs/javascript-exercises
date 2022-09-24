e41ca2de2b28ce6b7e113e1b6b6ca0d7a85d5ae3
August Dominik Raaschou-Pedersen <guciodevs@gmail.com>
2022-09-24 09:57:01 +0200

    Solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n < 0) return 'OOPS';
+    let index = 0;
+    if(typeof n === 'string'){
+        index = parseInt(n);
+    }else{
+        index = n;
+    }
+    return Math.round((1/Math.sqrt(5) * (((1+Math.sqrt(5))/2)**index - ((1-Math.sqrt(5))/2)**index)));
 };
 
 // Do not edit below this line
