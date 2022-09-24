0594b8c2916f8c20ea00337832566c3d81fc68ec
August Dominik Raaschou-Pedersen <guciodevs@gmail.com>
2022-09-24 10:20:42 +0200

    Solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+   const sortedByAge = people.sort(function(a,b){
+    const aAge = (a.hasOwnProperty('yearOfDeath')) ? a.yearOfDeath - a.yearOfBirth : 2022 - a.yearOfBirth;
+    const bAge = (b.hasOwnProperty('yearOfDeath')) ? b.yearOfDeath - b.yearOfBirth : 2022 - b.yearOfBirth;
+    if(aAge > bAge){
+        return -1;
+    }else{
+        return 1;
+    }
+   }); 
+
+   return sortedByAge[0];
 
 };
 
